78bf40c8c24b26f9a63b532a9f25bcd179411605
praraujo <pr.araujo@unesp.com>
2026-03-19 15:33:35 -0300

    Instalação de Speed e troca de icone

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { queryClient } from './app/query-client'
 import { router } from './app/router'
 import { Toaster } from '@/components/ui/sonner'
@@ -10,7 +11,6 @@ export function App() {
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <Toaster />
-      <Analytics />
     </QueryClientProvider>
   )
 }
